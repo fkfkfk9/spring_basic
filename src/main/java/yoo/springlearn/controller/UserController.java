323335099cd5dc3af4486eb0a8b3c73f9e3c4a0c
fkfkfk9 <fkfkfk999@gmail.com>
2021-07-07 23:58:20 +0900

    회원 입력 폼 작성
    컨트롤러와 폼만 작성

--- src/main/java/yoo/springlearn/controller/UserController.java
+++ src/main/java/yoo/springlearn/controller/UserController.java
@@ -2,6 +2,7 @@ package yoo.springlearn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import yoo.springlearn.service.UserService;
 
@@ -20,5 +21,10 @@ public class UserController {
      @Autowired
      public UserController(UserService userService){
          this.userService = userService;
+     }
+
+     @GetMapping("/users/join")
+     public String joinForm(){
+         return "users/createUserForm";
      }
 }
